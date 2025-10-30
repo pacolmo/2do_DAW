05cf4a8c81f8ffb22e3c5073620396b0dc105c07
pacolmo <fraolmseg@alu.edu.gva.es>
2025-10-30 13:51:35 +0100

    act interfaces

---
+++ Desarrollo web en entorno cliente/Ejemplos JS T-4/DOM/Ejemplos Dom/script.js
@@ -0,0 +1,9 @@
+const body = document.body;
+body.appendChild("Hello World","bye");
+
+const div = document.createElement("div");
+const strong = document.createElement("strong");
+div.innerText = "hello world";
+//body.append(div);
+div.textContent;
+console.log(div);
